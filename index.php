5e83de138408ab8cfa14939d3cc5e8cdf78a3e64
sgangadharan <sudeep.gangadharan@gmail.com>
2020-03-26 11:22:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1>Hello World! Welcome to the Tutorial on Design to Scale-2 by Sudeep Gangadharan</h1><br/><br/>";
+?>
+
+
+Session pages
+<p><a href="/session.php">Local (EC2) Session</a></p>
+<p><a href="/dyn_session.php">DynamoDb Session</a></p>
+
+Image pages
+<p><a href="/image.php">Image in EBS vol</a></p>
+<p><a href="/s3_image.php">Image in S3</a></p>
+
+
+</body>
+</html>
